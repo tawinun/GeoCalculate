56642ca81297d10c99a2e4b18c5968cb1fcd00b7
tawinun <s6070051@kmitl.ac.th>
2014-11-22 23:01:19 +0700

    calculate_area

---
+++ Project_python.py
@@ -0,0 +1,36 @@
+def square_area(side): #calculate square area
+      area = side*side
+      print area
+square_area(input())
+
+def rectangle_area(width, length): #calculate rectangle area
+      area = width*length
+      print area
+rectangle_area(input(), input())
+
+def paralellogram_area(base, height): #calculate paralellogram area
+      area = base*height
+      print area
+paralellogram_area(input(), input())
+
+def rhombus_kite_area(diagonal1, diagonal2): #calculate rhombus area
+      p_diagonal = diagonal1*diagonal2
+      area = (1/2)*p_diagonal
+      print area
+rhombus_kite_area(input(), input())trapezium
+
+def trapezium_area(diagonal, branch1, branch2): #calculate trapezium area
+      sum_branch = branch1+branch2
+      area = (1/2)*diagonal*sum_branch
+      print area
+trapezium_area(input(), input(), input())
+
+
+
+
+
+
+
+
+
+
