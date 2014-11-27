8d4c50e57ba65e9a4d7a0c635c9f9ec972580e3a
tawinun <s6070051@kmitl.ac.th>
2014-11-27 22:25:07 +0700

    Update Project_python.py
    v.2

--- Project_python.py
+++ Project_python.py
@@ -25,6 +25,90 @@ def trapezium_area(diagonal, branch1, branch2): #calculate trapezium area
       print area
 trapezium_area(input(), input(), input())
 
+def trapezoid(parallel_side1, parallel_side2, height):
+      area = (1/2.0)*height*(parallel_side1+parallel_side2)
+      print area
+trapezium_area(input(), input(), input()) 
+
+def triangle_area(base, height): #calculate triangle area
+      area = (1.0/2)*base*height
+      print area
+triangle_area(input(), input())
+
+def equilateral_triangle_area(side): #calculate equilateral triangle area
+      area = (sqrt(3)/4.0)*side*side
+      print area
+equilateral_triangle_area(input())
+
+def rightangled_triangle_area(cathetus1, cathetus2): #calculate rightangled triangle area
+      area = (1/2.0)*cathetus1*cathetus2
+      print area
+equilateral_triangle_area(input())
+
+def curved_base_triangular_area(degree, radian): #calculate curved base triangular area
+      area = (degree/360.0)*pi*(radian**2)
+      print area
+equilateral_triangle_area(input())
+
+def circle_area(radian):
+      area = float(pi*(radian**2))
+      print area
+circle_area(input())
+
+def circle_circumference(radian):
+      area = 2.0*pi*radian
+      print area
+circle_circumference(input())
+
+def sphere_surface_area(radian):
+      area = 4.0*pi*(radian**2)
+      print area
+sphere_surface_area(input())
+
+def circularcone_surface_area(radian, slant):
+      area = pi*radian*slant
+      print area
+circularcone_surface_area(input(), input())
+
+def cylinder_surfaceside_area(radian, height):
+      area = 2.0*pi*radian*height
+      print area
+cylinder_surface_area(input(), input())
+
+def sphere_volume(radian):
+      volume = (4.0/3)*pi*(radian**3)
+      print volume
+sphere_volume(input())
+
+def cylinder_volume(radian, height):
+      volume = pi*(radian**2)*float(height)
+      print volume
+cylinder_volume(input(), input())
+
+def cone_volume(radian, height)
+      volume = (1.0/3)*pi*(radian**2)*float(height)
+      print volume
+cone_volume(input(), input())
+
+def trapezoid_volume(parallel_side1, parallel_side2, height, height_prism):
+      volume = (1/2.0)*height*(parallel_side1+parallel_side2)*height_prism
+      print volume
+trapezium_volume(input(), input(), input(), input()) 
+
+def cube_volume(side):
+      volume = side**3.0
+      print volume
+cube_volume(input())
+
+def rectangle_prism_volume(width, length, height)
+      volume = width*length*height
+      print volume
+rectangle_prism_volume(input(), input(), input()) 
+
+
+
+
+
 
 
 
